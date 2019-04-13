6b56c04e7ccb99dc09eaa177797089d11e1cde6a
padudin <padudin@dasreda.ru>
2019-04-13 14:22:05 +0300

    000 - [add] - init project

---
+++ src/test/java/com/holidu/interview/DummyTest.java
@@ -0,0 +1,14 @@
+package com.holidu.interview;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+    // TODO: (padudin) del or use
+
+    @Test
+    public void someTest() {
+        Assert.assertTrue(true);
+    }
+}
